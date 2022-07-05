e5a064032449bc54be1f232673a7bc3ccc6e1524
nhuanh9 <chopperlouis@gmail.com>
2022-07-05 15:14:48 +0700

    done demo router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,39 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {LoginComponent} from "./login/login.component";
+import {AdminComponent} from "./admin/admin.component";
+import {HomeComponent} from "./home/home/home.component";
+import {ListProductComponent} from "./home/list-product/list-product.component";
+import {CreateProductComponent} from "./home/create-product/create-product.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent
+  },
+  {
+    path: 'admin',
+    component: AdminComponent
+  }, {
+    path: 'home',
+    component: HomeComponent,
+    children: [
+      {
+        path: 'list-product',
+        component: ListProductComponent
+      },
+      {
+        path: 'create-product',
+        component: CreateProductComponent
+      },
+    ]
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { AdminComponent } from './admin/admin.component';
+import { HomeComponent } from './home/home/home.component';
+import { ListProductComponent } from './home/list-product/list-product.component';
+import { CreateProductComponent } from './home/create-product/create-product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    AdminComponent,
+    HomeComponent,
+    ListProductComponent,
+    CreateProductComponent,
   ],
   imports: [
     BrowserModule,
